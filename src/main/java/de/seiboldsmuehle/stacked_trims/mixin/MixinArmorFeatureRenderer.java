f1aa2826fde738667b0dff4bbde6267be3c64881
AJ <adrian@seiboldsmuehle.de>
2023-02-10 15:05:14 +0100

    Fixed Loading Loop Bug

--- src/main/java/de/seiboldsmuehle/stacked_trims/mixin/MixinArmorFeatureRenderer.java
+++ src/main/java/de/seiboldsmuehle/stacked_trims/mixin/MixinArmorFeatureRenderer.java
@@ -26,6 +26,7 @@ import net.minecraft.registry.RegistryOps;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -42,19 +43,6 @@ public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extend
 
     @Shadow
     private final SpriteAtlasTexture armorTrimsAtlas;
-
-    @Redirect(method="renderArmor", at = @At(value="INVOKE", target = "Lnet/minecraft/item/trim/ArmorTrim;getTrim(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/item/ItemStack;)Ljava/util/Optional;"))
-    public Optional<ArmorTrim> getTrim(DynamicRegistryManager registryManager, ItemStack stack) {
-        if (stack.isIn(ItemTags.TRIMMABLE_ARMOR) && stack.getNbt() != null && stack.getNbt().contains("Trim")) {
-            NbtList nbtList = stack.getNbt().getList("Trim",10);
-            DataResult var10000 = ArmorTrim.CODEC.parse(RegistryOps.of(NbtOps.INSTANCE, registryManager), nbtList.get(0));
-            ArmorTrim armorTrim = (ArmorTrim)var10000.result().orElse((Object)null);
-            return Optional.ofNullable(armorTrim);
-        } else {
-            return Optional.empty();
-        }
-    }
-
     @Shadow
     private void renderTrim(ArmorMaterial armorMaterial, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, ArmorTrim armorTrim, boolean bl, A bipedEntityModel, boolean bl2, float f, float g, float h) {
         Sprite sprite = armorTrimsAtlas.getSprite(bl2 ? armorTrim.getLeggingsModelId(armorMaterial) : armorTrim.getGenericModelId(armorMaterial));
@@ -62,16 +50,32 @@ public abstract class MixinArmorFeatureRenderer<T extends LivingEntity, M extend
         bipedEntityModel.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, f, g, h, 1.0F);
     }
 
-    //METHOD CAUSES INFINITE LOOT CYCLE -> "locals = LocalCapture.CAPTURE_FAILSOFT" is Broken, find other way to change if content or fix capture
-/*
-    @Inject(method="renderArmor", at = @At(value="INVOKE", target = "Lnet/minecraft/world/World;getEnabledFeatures()Lnet/minecraft/resource/featuretoggle/FeatureSet;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
-    private void mixinRenderTrim(CallbackInfo ci, MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, ItemStack itemStack, ArmorItem armorItem, boolean bl, boolean bl2){
+
+    @Redirect(method="renderArmor", at = @At(value="INVOKE", target = "Lnet/minecraft/item/trim/ArmorTrim;getTrim(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/item/ItemStack;)Ljava/util/Optional;"))
+    public Optional<ArmorTrim[]> getTrim(DynamicRegistryManager registryManager, ItemStack stack) {
+        if (stack.isIn(ItemTags.TRIMMABLE_ARMOR) && stack.getNbt() != null && stack.getNbt().contains("Trim")) {
+            NbtList nbtList = stack.getNbt().getList("Trim",10);
+
+            DataResult[] dataResults = new DataResult[stack.getNbt().getList("Trim",10).size()-1];
+            ArmorTrim[] armorTrims = new ArmorTrim[stack.getNbt().getList("Trim",10).size()-1];
+            for(int i = 0; i < stack.getNbt().getList("Trim",10).size(); i++){
+                dataResults[i] = ArmorTrim.CODEC.parse(RegistryOps.of(NbtOps.INSTANCE, registryManager), nbtList.get(i));
+                armorTrims[i] = (ArmorTrim)dataResults[i].result().orElse((Object)null);
+            }
+
+            return Optional.ofNullable(armorTrims);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Inject(method="renderArmor", at = @At(value="INVOKE", target = "Lnet/minecraft/world/World;getEnabledFeatures()Lnet/minecraft/resource/featuretoggle/FeatureSet;"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void mixinRenderTrim(MatrixStack matrices, VertexConsumerProvider vertexConsumers, T entity, EquipmentSlot armorSlot, int light, A model, CallbackInfo ci, ItemStack itemStack, ArmorItem armorItem, boolean bl, boolean bl2){
         if (entity.world.getEnabledFeatures().contains(FeatureFlags.UPDATE_1_20)) {
-            ArmorTrim.getTrim(entity.world.getRegistryManager(), itemStack).ifPresent((armorTrim) -> {
-                renderTrim(armorItem.getMaterial(), matrices, vertexConsumers, light, armorTrim, bl2, model, bl, 1.0F, 1.0F, 1.0F);
+            ArmorTrim.getTrim(entity.world.getRegistryManager(), itemStack).ifPresent((armorTrims) -> {
+                renderTrim(armorItem.getMaterial(), matrices, vertexConsumers, light, armorTrims, bl2, model, bl, 1.0F, 1.0F, 1.0F);
             });
         }
         ci.cancel();
     }
-    */
 }
